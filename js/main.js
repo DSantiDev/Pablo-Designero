6e7b093969b0095a215802e73a93d097d42afbbe
David Santiago Rodriguez Moreno <davidsantiagorodriguez@hotmail.com>
2024-07-29 19:29:02 -0500

    [+] Se realizaron modificaciones al footer

--- js/main.js
+++ js/main.js
@@ -1,17 +1,25 @@
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
-const quitar = document.querySelector("#quitar")
+const abrir = document.getElementById('abrir');
+const cerrar = document.getElementById('cerrar');
+const nav = document.getElementById('nav');
+const quitar = document.getElementById('quitar');
 
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
-quitar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
-quitar.addEventListener("", () => {
-    nav.classList.remove("visible");
-})
+
+abrir.addEventListener('click', () => {
+    nav.classList.add('visible');
+});
+
+cerrar.addEventListener('click', () => {
+    nav.classList.remove('visible');
+});
+
+quitar.addEventListener('click', (event) => {
+    if (event.target.tagName === 'A') {
+        nav.classList.remove('visible');
+    }
+});
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 550) {
+        nav.classList.remove('visible');
+    }
+});
