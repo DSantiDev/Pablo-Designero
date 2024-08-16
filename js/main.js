a89a97051cfe9e65e815e31c9893252047e396fe
David Santiago Rodriguez Moreno <davidsantiagorodriguez@hotmail.com>
2024-08-15 21:21:53 -0500

    [+] Se creo el Redme

--- js/main.js
+++ js/main.js
@@ -1,25 +1,24 @@
-// const abrir = document.getElementById('abrir');
-// const cerrar = document.getElementById('cerrar');
-// const nav = document.getElementById('nav');
-// const quitar = document.getElementById('quitar');
+const abrir = document.getElementById('abrir');
+const cerrar = document.getElementById('cerrar');
+const nav = document.getElementById('nav');
+const quitar = document.getElementById('quitar');
 
 
-// abrir.addEventListener('click', () => {
-//     nav.classList.add('visible');
-// });
+abrir.addEventListener('click', () => {
+    nav.classList.add('visible');
+});
 
-// cerrar.addEventListener('click', () => {
-//     nav.classList.remove('visible');
-// });
+cerrar.addEventListener('click', () => {
+    nav.classList.remove('visible');
+ });
 
-// quitar.addEventListener('click', (event) => {
-//     if (event.target.tagName === 'A') {
-//         nav.classList.remove('visible');
-//     }
-// });
+quitar.addEventListener('click', (event) => {
+    if (event.target.tagName === 'A') {
+        nav.classList.remove('visible');
+    }
+});
 
-// window.addEventListener('resize', () => {
-//     if (window.innerWidth >= 550) {
-//         nav.classList.remove('visible');
-//     }
-// });
+window.addEventListener('resize', () => {
+    if (window.innerWidth >= 550) {
+        nav.classList.remove('visible');
+} });
---
+++ readme.md
@@ -0,0 +1,29 @@
+**Proyecto hecho por**: ***David Santiago Rodriguez Moreno***
+# Pablo Designero
+Simulación de un portafolio para un diseñador usando `HTML`, `CSS` y `JavaScript`
+
+## Conceptos trabajados en este proyecto
+A continuación los siguientes conceptos son los que se implementaron en este proyecto 
+
+- BEM: Metodología `CSS`
+- ITCSS: Arquitectura de `CSS`
+- Mobile First: Metodología de desarollo
+## Características del proyecto 
+- Menú despegable
+- Formulario, guarda datos en LocalStorage
+
+## Propósito
+El propósito de esta plantailla es ejemplificar la manera en la que se construye un sitio web usando `HTML`, `CSS` y `JavaScript`.
+
+[Ir a Google](http://google.com)
+
+```
+console.log("Pablo Designero")
+
+```
+## Equipo
+
+| Nombre          | Ciudad |
+| --------------- | ------ |
+| David Rodriguez | Bogotá |
+| Duvan Lopez     | Bogotá |
